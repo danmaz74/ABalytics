4f372e65dcf6e4e5dcc63aaf8317b3bff39f0935
Alexander <alexander@getclicky.com>
2013-06-19 15:42:51 -0700

    Implemented rudimentary modularisation, and subsequently added in Clicky support.

--- abalytics.js
+++ abalytics.js
@@ -19,9 +19,24 @@
 
 var ABalytics = {
     changes: [],
+    cont: {},
     // for each experiment, load a variant if already saved for this session, or pick a random one
-    init: function(config, __gaq, start_slot) {
+    init: function(config, __cont, start_slot) {
         if (typeof(start_slot) == 'undefined') start_slot = 1;
+
+        this.cont = __cont;
+
+        if (this.cont.length !== 2)
+            return;
+
+        if (this.cont[0] == 'Clicky')
+            this.amend = this.amendClicky;
+        else if (this.cont[0] == 'Google')
+            this.amend = this.amendGoogle;
+        else
+            return;
+
+        this.cont = this.cont[1];
 
         for (var experiment in config) {
             var variants = config[experiment];
@@ -36,20 +51,35 @@ var ABalytics = {
 
             var variant = variants[variant_id];
 
-            // ga.js changes _gaq into an object with a custom push() method but no concat,
-            // so we have to push each _setCustomVar individually
-            __gaq.push(['_setCustomVar',
-                start_slot,
+            this.amend(start_slot,
                 experiment,                 // The name of the custom variable = name of the experiment
                 variant.name,               // The value of the custom variable = variant shown
                 2                           // Sets the scope to session-level
-            ]);
+            );
+
             start_slot++;
 
             for (var change in variant) {
                 if (change != 'name') this.changes.push([change,variant[change]]);
             }
         }
+    },
+    // Clicky-specific amend
+    amendClicky: function(slot, exp, var_name, scope) {
+        this.cont.split = {
+            name: exp,
+            version: var_name,
+        };
+    },
+    amendGoogle: function(slot, exp, var_name, scope) {
+        // ga.js changes _gaq into an object with a custom push() method but no concat,
+        // so we have to push each _setCustomVar individually
+        this.cont.push(['_setCustomVar',
+            slot,
+            exp,
+            var_name,
+            scope
+        ]);
     },
     // apply the selected variants for each experiment
     applyHtml: function() {
