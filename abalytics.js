f6abfa85f41ca5bf399d8d92e5f905614f24dacc
George Zografos <george.p.zografos@gmail.com>
2014-10-12 01:34:37 +0300

    Wrap in IIFE.

--- abalytics.js
+++ abalytics.js
@@ -17,72 +17,76 @@
 // BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
 // OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-var ABalytics = {
-    changes: [],
-    // for each experiment, load a variant if already saved for this session, or pick a random one
-    init: function(config, __gaq, start_slot) {
-        if (typeof(start_slot) == 'undefined') start_slot = 1;
+var ABalytics = (function (window, document, undefined) {
+    return {
+        changes: [],
+        // for each experiment, load a variant if already saved for this session, or pick a random one
+        init: function (config, __gaq, start_slot) {
+            if (typeof (start_slot) == 'undefined') start_slot = 1;
 
-        for (var experiment in config) {
-            var variants = config[experiment];
+            for (var experiment in config) {
+                var variants = config[experiment];
 
-            // read the saved variant for this experiment in this session, or pick a random one and save it
-            var variant_id = this.readCookie("ABalytics_"+experiment);
-            if (!variant_id || !variants[variant_id]) {
-                // pick a random variant
-                variant_id = Math.floor(Math.random()*variants.length);
-                document.cookie = "ABalytics_"+experiment+"="+variant_id+"; path=/";
+                // read the saved variant for this experiment in this session, or pick a random one and save it
+                var variant_id = this.readCookie("ABalytics_" + experiment);
+                if (!variant_id || !variants[variant_id]) {
+                    // pick a random variant
+                    variant_id = Math.floor(Math.random() * variants.length);
+                    document.cookie = "ABalytics_" + experiment + "=" + variant_id + "; path=/";
+                }
+
+                var variant = variants[variant_id];
+
+                // ga.js changes _gaq into an object with a custom push() method but no concat,
+                // so we have to push each _setCustomVar individually
+                __gaq.push(['_setCustomVar',
+                    start_slot,
+                    experiment, // The name of the custom variable = name of the experiment
+                    variant.name, // The value of the custom variable = variant shown
+                    2 // Sets the scope to session-level
+                ]);
+                start_slot++;
+
+                for (var change in variant) {
+                    if (change != 'name') this.changes.push([change, variant[change]]);
+                }
+            }
+        },
+        // apply the selected variants for each experiment
+        applyHtml: function () {
+            for (var i = 0; i < this.changes.length; i++) {
+                var change = this.changes[i];
+                var elements = document.getElementsByClassName ? document.getElementsByClassName(
+                    change[0]) : this.getElementsByClassName(change[0]);
+
+                for (var j = 0; j < elements.length; j++) elements[j].innerHTML = change[1];
+            }
+        },
+        readCookie: function (name) {
+            var nameEQ = name + "=";
+            var ca = document.cookie.split(';');
+            for (var i = 0; i < ca.length; i++) {
+                var c = ca[i];
+                while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+                if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+            }
+            return null;
+        },
+        getElementsByClassName: function (className) {
+            var hasClassName = new RegExp("(?:^|\\s)" + className + "(?:$|\\s)");
+            var allElements = document.getElementsByTagName("*");
+            var results = [];
+
+            var element;
+            for (var i = 0;
+                ((element = allElements[i]) != null) && (element != undefined); i++) {
+                var elementClass = element.className;
+                if (elementClass && elementClass.indexOf(className) != -1 && hasClassName.test(
+                    elementClass))
+                    results.push(element);
             }
 
-            var variant = variants[variant_id];
-
-            // ga.js changes _gaq into an object with a custom push() method but no concat,
-            // so we have to push each _setCustomVar individually
-            __gaq.push(['_setCustomVar',
-                start_slot,
-                experiment,                 // The name of the custom variable = name of the experiment
-                variant.name,               // The value of the custom variable = variant shown
-                2                           // Sets the scope to session-level
-            ]);
-            start_slot++;
-
-            for (var change in variant) {
-                if (change != 'name') this.changes.push([change,variant[change]]);
-            }
+            return results;
         }
-    },
-    // apply the selected variants for each experiment
-    applyHtml: function() {
-        for (var i=0;i<this.changes.length;i++) {
-            var change = this.changes[i];
-            var elements = document.getElementsByClassName ? document.getElementsByClassName(change[0]) : this.getElementsByClassName(change[0]);
-
-            for (var j=0;j<elements.length;j++) elements[j].innerHTML = change[1];
-        }
-    },
-    readCookie: function(name) {
-        var nameEQ = name + "=";
-        var ca = document.cookie.split(';');
-        for(var i=0;i < ca.length;i++) {
-            var c = ca[i];
-            while (c.charAt(0)==' ') c = c.substring(1,c.length);
-            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-        }
-        return null;
-    },
-    getElementsByClassName: function(className)
-    {
-        var hasClassName = new RegExp("(?:^|\\s)" + className + "(?:$|\\s)");
-        var allElements = document.getElementsByTagName("*");
-        var results = [];
-
-        var element;
-        for (var i = 0; ((element = allElements[i]) != null) && (element != undefined); i++) {
-            var elementClass = element.className;
-            if (elementClass && elementClass.indexOf(className) != -1 && hasClassName.test(elementClass))
-                results.push(element);
-        }
-
-        return results;
-    }
-};
+    };
+})(window, document);
